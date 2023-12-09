d0f389df8a1b6ef8644b3206211d72fd64193b6f
Rakandhiya Daanii Rachmanto <rakandhiya.daanii@gmail.com>
2023-12-09 19:06:51 +0700

    Add nerd-icons-corfu for emacs completions

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("cd51c4e5a258ab9fd82de1fb4c0eee54326de5e307e3bf2895467ae103bc562b" default))
+   '("cbd85ab34afb47003fa7f814a462c24affb1de81ebf172b78cb4e65186ba59d2" "8f5b54bf6a36fe1c138219960dd324aad8ab1f62f543bed73ef5ad60956e36ae" "ffba0482d3548c9494e84c1324d527f73ea4e43fff8dfd0e48faa8fc6d5c2bc7" "d0fd069415ef23ccc21ccb0e54d93bdbb996a6cce48ffce7f810826bb243502c" "cd51c4e5a258ab9fd82de1fb4c0eee54326de5e307e3bf2895467ae103bc562b" default))
+ '(nil nil t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -12,4 +13,4 @@
  ;; If there is more than one, they won't work right.
  '(marginalia-documentation ((t (:inherit font-lock-doc-face))))
  '(neo-root-dir-face ((t (:background "#212121" :foreground "#c3e88d" :box nil))))
- '(persp-selected-face ((t (:foreground "#82aaff" :weight bold))))))
+ '(persp-selected-face ((t (:foreground "#82aaff" :weight bold)))))
