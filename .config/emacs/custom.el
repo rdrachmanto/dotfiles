07d57944e6bd8e16444205406352de3486ba402c
Rakandhiya Daanii Rachmanto <rakandhiya.daanii@gmail.com>
2023-12-09 15:52:49 +0700

    Change emacs to use elpaca package manager

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -5,8 +5,6 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("cd51c4e5a258ab9fd82de1fb4c0eee54326de5e307e3bf2895467ae103bc562b" default))
- '(package-selected-packages
-   '(minions latex-preview-pane auctex anzu tangonov-theme evil-anzu apropospriate-theme moody nordic-night org-modern go-mode lua-mode evil-collection embark consult elixir-mode marginalia vertico eldoc-box magit which-key treesit-auto rainbow-delimiters evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,4 +12,4 @@
  ;; If there is more than one, they won't work right.
  '(marginalia-documentation ((t (:inherit font-lock-doc-face))))
  '(neo-root-dir-face ((t (:background "#212121" :foreground "#c3e88d" :box nil))))
- '(persp-selected-face ((t (:foreground "#82aaff" :weight bold)))))
+ '(persp-selected-face ((t (:foreground "#82aaff" :weight bold))))))
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,12 +1,3 @@
-;; Configure the package manager
-;; For some reason this cannot be done in the .org file
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-
-;; Set use-package to always ensure that a package specified in the config is installed
-(require 'use-package-ensure)
-(setq use-package-always-ensure t)
-
 ;; Read and tangle the emacs-config.org file
 (require 'org)
 (org-babel-load-file
