b855879513846198aadff04d1b949a3a9a46a8e0
Rakandhiya Daanii Rachmanto <rakandhiya.daanii@gmail.com>
2023-12-11 19:24:48 +0700

    Adjust wezterm colors to reflect tangonov better

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,7 +7,41 @@ local config = wezterm.config_builder()
 config.color_scheme = "Material Darker (base16)"
 config.colors = {
   cursor_bg = "#ffca41",
-  background = "#151515"
+  background = "#151515",
+  tab_bar = {
+    background = "#151515",
+    active_tab = {
+      bg_color = "#232323",
+      fg_color = "#EEFFFF",
+      intensity = "Bold",
+    },
+    inactive_tab = {
+      bg_color = "#232323",
+      fg_color = "#626262"
+    },
+    inactive_tab_hover = {
+      bg_color = "#232323",
+      fg_color = "#626262",
+      italic = true,
+    },
+    new_tab = {
+      bg_color = "#232323",
+      fg_color = "#626262"
+    },
+    new_tab_hover = {
+      bg_color = "#232323",
+      fg_color = "#626262",
+      italic = true,
+    },
+  }
+}
+
+-- window padding
+config.window_padding = {
+  top = 6,
+  left = 6,
+  right = 6,
+  bottom = 2,
 }
 
 -- fonts
