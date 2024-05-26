b5723bfddba3a742a752d9fecfd3ef3ebbebb4de
Rakandhiya Daanii Rachmanto <rakandhiya.daanii@gmail.com>
2024-05-26 14:50:32 +0700

    remigrate emacs from straight to elpaca

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,4 +1,5 @@
 (defvar default-file-name-handler-alist file-name-handler-alist)
-(setq package-enable-at-startup nil
+(setq package-enable-at-startup t
       file-name-handler-alist nil
       gc-cons-threshold (* 50 1000 1000))
+
