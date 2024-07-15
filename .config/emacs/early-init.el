4ab7d92554b837f1ada08f7a9fbbb788d08269e8
Rakandhiya Daanii Rachmanto <rakandhiya.daanii@gmail.com>
2024-07-15 10:28:27 +0700

    Fix issue package-el starting before elpaca

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,5 +1,5 @@
 (defvar default-file-name-handler-alist file-name-handler-alist)
-(setq package-enable-at-startup t
+(setq package-enable-at-startup nil
       file-name-handler-alist nil
       gc-cons-threshold (* 50 1000 1000))
 
