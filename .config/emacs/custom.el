e22312ecf0bca3c793a6510e9b41cc3998aed359
Rakandhiya Rachmanto <rakandhiya.daanii@gmail.com>
2025-04-12 02:30:08 -0400

    emacs: change emacs font

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(doom-modeline embark-consult embark rainbow-delimiter rainbow-delimiters nerd-icons-corfu nerd-icons-completion nerd-icons which-key org-modern marginalia consult vertico evil-collection evil-anzu anzu evil cape corfu)))
+   '(markdown-mode eldoc-box exec-path-from-shell tree-sitter-langs doom-themes doom-modeline embark-consult embark rainbow-delimiter rainbow-delimiters nerd-icons-corfu nerd-icons-completion nerd-icons which-key org-modern marginalia consult vertico evil-collection evil-anzu anzu evil cape corfu)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,3 +1,4 @@
+;; Use package settings
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
         ("gnu"   . "https://elpa.gnu.org/packages/")))
@@ -5,9 +6,18 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+
+;; Set path from shell
+(use-package exec-path-from-shell)
+
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+
 (require 'org)
 (org-babel-load-file
   (expand-file-name "config.org" user-emacs-directory))
+
 
 ;; Set the file to write custom-configs from Emacs and load it
 (setq custom-file (concat user-emacs-directory "custom.el"))
