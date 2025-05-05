8a7b55fa67d1eaff361c8664593c3d27da2e68b8
Rakandhiya Rachmanto <rakandhiya.daanii@gmail.com>
2025-05-05 14:11:55 -0400

    nvim: swap defaults to go earlier than packages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,89 @@ require("scripts.autocmd")
 
 
 -- --------------------------------------------------------------
+-- Defaults 
+-- --------------------------------------------------------------
+-- Set some default options for the following:
+-- 1. Buffer elements
+-- 2. Floating window
+-- 3. Colorscheme
+-- 4. Statusline and winbar
+-- 5. LSP config
+-- 6. Diagnostics
+-- --------------------------------------------------------------
+
+local o = vim.o
+local g = vim.g
+
+g.netrw_banner=0
+g.netrw_browse_split=4
+g.netrw_altv=1
+g.netrw_liststyle=3
+
+o.clipboard = "unnamedplus"
+o.splitbelow = true
+o.splitright = true
+
+o.termguicolors=true
+
+o.scrolloff=5
+o.cursorline=true
+o.number=true
+o.numberwidth=3
+o.relativenumber=true
+o.showtabline=2
+
+o.autoindent=true
+o.expandtab=true
+o.tabstop=2
+o.shiftwidth=2
+o.signcolumn="yes"
+o.foldcolumn="1"
+
+o.ignorecase=true
+o.smartcase=true
+
+o.winborder="solid" -- Options: single, double, rounded, solid, shadow
+
+vim.opt.fillchars={ eob=' ' }
+
+-- Statusline and winbar
+vim.o.laststatus = 3
+vim.o.winbar = " %t %{%v:lua.require'nvim-navic'.get_location()%}"
+
+-- LSP
+vim.lsp.config("*", {
+  root_markers = { ".git" }
+})
+
+vim.lsp.enable({ "lua_ls", "basedpyright" })
+
+-- Diagnostic signs
+vim.diagnostic.config({
+  virtual_text = false,
+  underline = {
+    severity = {
+      min = vim.diagnostic.severity.HINT,
+    }
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "▲",
+      [vim.diagnostic.severity.WARN] = "●",
+      [vim.diagnostic.severity.INFO] = "○",
+      [vim.diagnostic.severity.HINT] = "○",
+    },
+    texthl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+      [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+      [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+      [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+    }
+  }
+})
+
+
+-- --------------------------------------------------------------
 -- Plugins
 -- --------------------------------------------------------------
 -- Plugins declared via lazy.nvim
@@ -116,8 +199,9 @@ local util_pkgs = {
         "fzf-native",
         fzf_colors = true,
         winopts = {
+          border = vim.o.winborder,
           preview = {
-            -- border = "single",
+            border = vim.o.winborder,
             delay = 250,
           }
         },
@@ -133,29 +217,30 @@ local util_pkgs = {
 }
 
 local colorschemes = {
-  -- {
-  --   "rdrachmanto/cisco-theme.nvim",
-  --   config = function()
-  --     require("cisco").setup({
-  --       contrast = {
-  --         floating_windows = true
-  --       }
-  --     })
-  --   end
-  -- },
   {
-    dir = "~/Devel/cisco-theme.nvim",
-    name = "cisco",
-    lazy = false,
-    priority = 1000,
+    "rdrachmanto/cisco-theme.nvim",
     config = function()
       require("cisco").setup({
         contrast = {
           floating_windows = true
         }
       })
-    end,
-  }
+      vim.cmd.colorscheme("cisco")
+    end
+  },
+  -- {
+  --   dir = "~/Devel/cisco-theme.nvim",
+  --   name = "cisco-theme.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("cisco").setup({
+  --       contrast = {
+  --         floating_windows = true
+  --       }
+  --     })
+  --   end,
+  -- }
 }
 
 local enabled_pkgs = utils.package_loader(
@@ -163,92 +248,6 @@ local enabled_pkgs = utils.package_loader(
 )
 require("lazy").setup({
   enabled_pkgs
-})
-
-
--- --------------------------------------------------------------
--- Defaults 
--- --------------------------------------------------------------
--- Set some default options for the following:
--- 1. Buffer elements
--- 2. Floating window
--- 3. Colorscheme
--- 4. Statusline and winbar
--- 5. LSP config
--- 6. Diagnostics
--- --------------------------------------------------------------
-
-local o = vim.o
-local g = vim.g
-
-g.netrw_banner=0
-g.netrw_browse_split=4
-g.netrw_altv=1
-g.netrw_liststyle=3
-
-o.clipboard = "unnamedplus"
-o.splitbelow = true
-o.splitright = true
-
-o.termguicolors=true
-
-o.scrolloff=5
-o.cursorline=true
-o.number=true
-o.numberwidth=3
-o.relativenumber=true
-o.showtabline=2
-
-o.autoindent=true
-o.expandtab=true
-o.tabstop=2
-o.shiftwidth=2
-o.signcolumn="yes"
-o.foldcolumn="1"
-
-o.ignorecase=true
-o.smartcase=true
-
-o.winborder="single" -- Options: single, double, rounded, solid, shadow
-
-vim.opt.fillchars={ eob=' ' }
-
--- Colorscheme
-vim.cmd("colorscheme cisco")
-
--- Statusline and winbar
-vim.o.laststatus = 3
-vim.o.winbar = " %t %{%v:lua.require'nvim-navic'.get_location()%}"
-
--- LSP
-vim.lsp.config("*", {
-  root_markers = { ".git" }
-})
-
-vim.lsp.enable({ "lua_ls", "basedpyright" })
-
--- Diagnostic signs
-vim.diagnostic.config({
-  virtual_text = false,
-  underline = {
-    severity = {
-      min = vim.diagnostic.severity.HINT,
-    }
-  },
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = "▲",
-      [vim.diagnostic.severity.WARN] = "●",
-      [vim.diagnostic.severity.INFO] = "○",
-      [vim.diagnostic.severity.HINT] = "○",
-    },
-    texthl = {
-      [vim.diagnostic.severity.ERROR] = "DiagnosticError",
-      [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
-      [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
-      [vim.diagnostic.severity.HINT] = "DiagnosticHint",
-    }
-  }
 })
 
 
