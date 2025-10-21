a8efc074cb278638d59496f6ba0d5cf8879cab7a
Rakandhiya Rachmanto <rakandhiya.daanii@gmail.com>
2025-10-21 01:26:03 -0400

    emacs: rewrite config

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -1,13 +1,10 @@
+;;; -*- lexical-binding: t -*-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("604afb0a32766a27ba949d6a29923c8241f5f3c9a05f1680d14740d5eacb9b03"
-     "408cb53860c67e4e7bc2ec5b7ce652831969965fe516ccd7507e4ea6e79dd6cc"
-     default))
- '(package-selected-packages nil))
+ '(package-selected-packages '(consult corfu diff-hl meow rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,25 +1,75 @@
-;; Use package settings
-(setq package-archives
-      '(("melpa" . "https://melpa.org/packages/")
-        ("gnu"   . "https://elpa.gnu.org/packages/")))
+(add-to-list 'load-path
+	     (expand-file-name "lisp/" user-emacs-directory))
 
-(require 'use-package)
-(setq use-package-always-ensure t)
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
+(setq auto-save-default nil
+      make-backup-files nil)
 
-;; Set path from shell
-(use-package exec-path-from-shell)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
+(global-display-line-numbers-mode)
+(global-hl-line-mode 1)
+(menu-bar--display-line-numbers-mode-relative)
+(fido-vertical-mode t)
 
+(setq tab-always-indent 'complete)
 
-(require 'org)
-(org-babel-load-file
-  (expand-file-name "config.org" user-emacs-directory))
+(load-theme 'modus-vivendi t)
 
+(setq ring-bell-function 'ignore)
 
-;; Set the file to write custom-configs from Emacs and load it
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(when (file-exists-p custom-file)
-  (load-file custom-file))
+(add-to-list 'default-frame-alist '(font . "Zed Mono Nerd Font-11"))
+
+(setq read-file-name-completion-ignore-case t
+      read-buffer-completion-ignore-case t)
+
+(setq minibuffer-visible-completions t
+      resize-mini-windows t
+      max-mini-window-height 0.15)
+
+(windmove-default-keybindings)
+
+;; Packages
+(use-package corfu
+  :ensure t
+  :init
+  (global-corfu-mode)
+  :config
+  (setq corfu-cycle t
+	corfu-quit-at-boundary nil
+	corfu-quit-no-match nil
+	corfu-on-exact-match nil)
+  ;; Autocompletion settings
+  (setq corfu-auto t
+	corfu-auto-delay 0.3
+	corfu-auto-prefix 3))
+
+;; Programming modes
+(use-package rust-mode
+  :ensure t)
+
+;; Modal editing
+(use-package meow
+  :ensure t
+  :config
+  (require 'meow-keybinds)  ;; Keybinds for movement and others defined here
+  (meow-setup)
+  (meow-global-mode 1))
+
+;; (use-package evil
+;;   :ensure t
+;;   :config (evil-mode 1))
+
+(use-package consult
+  :ensure t)
+
+(use-package eglot
+  :hook
+  (prog-mode . eglot-ensure))
+
+(use-package diff-hl
+  :ensure t
+  :config (global-diff-hl-mode))
