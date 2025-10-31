70ab184403222c830127a9b9abe43fcb223d982c
Rakandhiya Rachmanto <rakandhiya.daanii@gmail.com>
2025-10-31 00:46:20 -0400

    remove custom statusline from nvim

--- .config/nvim/lua/scripts/autocmd.lua
+++ .config/nvim/lua/scripts/autocmd.lua
@@ -6,12 +6,6 @@ local statusline_evts = {
   "LspRequest",
 }
 
-vim.api.nvim_create_autocmd(statusline_evts, {
-  callback = function ()
-    vim.opt.statusline = require("scripts.statusline").build_statusline()
-  end
-})
-
 vim.api.nvim_create_autocmd({"BufEnter", "VimEnter"}, {
   callback = function ()
     vim.opt.winbar = require("scripts.winbar").set_winbar()
